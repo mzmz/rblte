9c73fd1efe614edcc58348fcb17e6ddb59f204ec
mzmz <milton.zhumingzhen@gmail.com>
2014-03-11 18:49:29 +0800

    shorten the filenames

---
+++ src/log_item_rspec.rb
@@ -0,0 +1,15 @@
+
+require_relative 'lte_log_item'
+
+describe LteMessage do
+  before :each do
+    @lteMessage = LteMessage.new
+  end
+
+  it "checks fields and values of an item according to the message definition"
+  
+  it "map an log item to message"
+  it "should diff log item and message definition"
+  it "should"
+end
+
---
+++ src/msg_def_rspec.rb
@@ -0,0 +1,6 @@
+require_relative 'lte_msg_def'
+
+describe LteMsgDef do
+  it "includes all message definitions in in ts36.331 and ts24.301"
+  it "lists the message tree"
+end
